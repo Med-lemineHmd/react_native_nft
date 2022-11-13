d6950d5ca298652a7e31f2770abf42a121c618d6
Mohamed lamine HAMDINOU <mohamedlaminehamdinou@Mohameds-MacBook-Pro.local>
2022-11-13 22:17:30 +0100

    Create NFT Details

--- components/index.js
+++ components/index.js
@@ -2,5 +2,17 @@ import FocusedStatusBar from "./FocusedStatusBar";
 import HomeHeader from "./HomeHeader";
 import NFTCard from "./NFTCard";
 import { CircleButton, RectButton } from "./Button";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
 
-export { FocusedStatusBar, HomeHeader, NFTCard, CircleButton, RectButton };
+export {
+  FocusedStatusBar,
+  HomeHeader,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  DetailsDesc,
+  DetailsBid,
+  SubInfo,
+};
